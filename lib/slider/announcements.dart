1d25cd863ac30d243fbb4fa372fc0a155af20dd0
ofektabak <tabako@post.bgu.ac.il>
2021-09-05 00:07:44 +0300

    hola chikita

--- lib/slider/announcements.dart
+++ lib/slider/announcements.dart
@@ -1,11 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../database.dart';
 
+// class Announcements extends StatefulWidget {
+//   @override
+//   _AnnouncementsState createState() => _AnnouncementsState();
+// }
+
+// class _AnnouncementsState extends State<Announcements> {
+//   @override
+//   void initState() {
+//     super.initState();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Announcements"),
+//         centerTitle: true,
+//       ),
+//       body: Column(
+//         children: [],
+//       ),
+//     );
+//   }
+// }
 class Announcements extends StatefulWidget {
   @override
   _AnnouncementsState createState() => _AnnouncementsState();
 }
 
 class _AnnouncementsState extends State<Announcements> {
+  Widget chatRoomList() {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('Announcements')
+          .orderBy("date", descending: true)
+          .snapshots(),
+      builder: (context, snapshot) {
+        return snapshot.hasData
+            ? ListView.builder(
+                itemCount: snapshot.data.docs.length,
+                itemBuilder: (context, index) {
+                  return ChatRoomTile(
+                    snapshot.data.docs[index].data()['announcement'],
+                    snapshot.data.docs[index].data()['date'],
+                  );
+                })
+            : Container();
+      },
+    );
+  }
+
   @override
   void initState() {
     super.initState();
@@ -15,12 +62,90 @@ class _AnnouncementsState extends State<Announcements> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Announcements"),
+        title: Text(
+          "Announcements",
+        ),
         centerTitle: true,
+        automaticallyImplyLeading: false,
       ),
-      body: Column(
-        children: [],
+      body: Container(
+        child: chatRoomList(),
       ),
+    );
+  }
+}
+
+// class ChatRoomTile extends StatelessWidget {
+//   final String message;
+//   final String date;
+//   ChatRoomTile(
+//     this.message,
+//     this.date,
+//   );
+//   @override
+//   Widget build(BuildContext context) {
+//     return GestureDetector(
+//       onTap: () {
+//         print("damn");
+//       },
+//       child: Container(
+//           padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+//           decoration:
+//               BoxDecoration(border: Border.all(color: Colors.grey[300])),
+//           child: Row(
+//             children: [
+//               SizedBox(
+//                 width: 12,
+//               ),
+//               Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+//                 Text(message),
+//                 Text(date),
+//               ]),
+//             ],
+//           )),
+//     );
+//   }
+// }
+class ChatRoomTile extends StatelessWidget {
+  final String message;
+  final Timestamp date;
+  ChatRoomTile(
+    this.message,
+    this.date,
+  );
+  @override
+  Widget build(BuildContext context) {
+    String fulldate = date.toDate().day.toString() +
+        "/" +
+        date.toDate().month.toString() +
+        "/" +
+        date.toDate().year.toString() +
+        "\n" +
+        date.toDate().hour.toString() +
+        ":" +
+        date.toDate().minute.toString();
+    return GestureDetector(
+      onTap: () {
+        print("damn");
+      },
+      child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          decoration:
+              BoxDecoration(border: Border.all(color: Colors.grey[300])),
+          child: Column(children: [
+            Align(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  message,
+                  textDirection: TextDirection.rtl,
+                )),
+            Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  fulldate,
+                  textDirection: TextDirection.ltr,
+                )),
+          ])),
     );
   }
 }
